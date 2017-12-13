53d89b00874e5cb1933efd659e8cb0e6d47b2ea9
To Anh Vu <vu.to@mobytelab.com>
2017-12-13 17:02:28 +0700

    init with IQKeyboardManager

---
+++ MLKit/MLKit/MLKit.h
@@ -0,0 +1,18 @@
+//
+//  MLKit.h
+//  MLKit
+//
+//  Created by Anh Vu on 12/13/17.
+//  Copyright © 2017 Mobytelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for MLKit.
+FOUNDATION_EXPORT double MLKitVersionNumber;
+
+//! Project version string for MLKit.
+FOUNDATION_EXPORT const unsigned char MLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLKit/PublicHeader.h>
+
+
